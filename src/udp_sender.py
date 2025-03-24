7d5803aa586c5a81f0599b702d6db776da94cda9
julienvdh <julien.vanderheyden@student.uliege.be>
2025-03-24 10:42:33 +0100

    Addition of udp_sender.py

---
+++ src/udp_sender.py
@@ -0,0 +1,15 @@
+import socket
+
+# Sender settings
+HOST = '127.0.0.1'  # IP address of the receiver (localhost in this case)
+PORT = 12345        # UDP port to send data to
+
+# Create UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Send message
+message = "Hello, Julia! (FROM PYTHON)"
+sock.sendto(message.encode(), (HOST, PORT))
+
+print(f"Sent message: {message} to {HOST}:{PORT}")
+sock.close()
