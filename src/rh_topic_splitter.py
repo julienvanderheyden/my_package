5af120e69dd45c156affba9441fed1cbf3db5510
julienvdh <julien.vanderheyden@student.uliege.be>
2025-03-26 14:23:14 +0100

    Modify topic splitter to handle joints coupling

--- src/rh_topic_splitter.py
+++ src/rh_topic_splitter.py
@@ -8,12 +8,11 @@ class DynamicRouter:
         rospy.init_node('dynamic_router', anonymous=True)
 
         self.sub_topic = "/shadowhand_command_topic"
-        self.pub_topics = ["/sh_rh_wrj1_position_controller/command", "/sh_rh_wrj2_position_controller/command", "/fake_topic",
+        self.pub_topics = ["/sh_rh_wrj1_position_controller/command", "/sh_rh_wrj2_position_controller/command",
                            "/sh_rh_ffj0_position_controller/command", "/sh_rh_ffj3_position_controller/command", "/sh_rh_ffj4_position_controller/command", 
-                           "/fake_topic", "/sh_rh_mfj0_position_controller/command", "/sh_rh_mfj3_position_controller/command", "/sh_rh_mfj4_position_controller/command", 
-                           "/fake_topic", "/sh_rh_rfj0_position_controller/command", "/sh_rh_rfj3_position_controller/command",
-                           "/sh_rh_rfj4_position_controller/command", "/fake_topic", "/sh_rh_lfj0_position_controller/command", 
-                           "/sh_rh_lfj3_position_controller/command", "/sh_rh_lfj4_position_controller/command",  
+                           "/sh_rh_mfj0_position_controller/command", "/sh_rh_mfj3_position_controller/command", "/sh_rh_mfj4_position_controller/command", 
+                           "/sh_rh_rfj0_position_controller/command", "/sh_rh_rfj3_position_controller/command", "/sh_rh_rfj4_position_controller/command", 
+                           "/sh_rh_lfj0_position_controller/command", "/sh_rh_lfj3_position_controller/command", "/sh_rh_lfj4_position_controller/command",  
                            "/sh_rh_lfj5_position_controller/command", "/sh_rh_thj1_position_controller/command", "/sh_rh_thj2_position_controller/command", 
                            "/sh_rh_thj3_position_controller/command", "/sh_rh_thj4_position_controller/command", "/sh_rh_thj5_position_controller/command"]
 
@@ -32,6 +31,8 @@ class DynamicRouter:
         self.joints_names = ["WRJ1", "WRJ2", "FFJ1", "FFJ2", "FFJ3", "FFJ4", "MFJ1", "MFJ2", "MFJ3", "MFJ4", "RFJ1", "RFJ2", "RFJ3", "RFJ4",
                             "LFJ1", "LFJ2", "LFJ3", "LFJ4", "LFJ5", "THJ1", "THJ2", "THJ3", "THJ4", "THJ5"]
 
+        self.coupled_fingers = {2,5,8, 11}
+
         self.rate = rospy.Rate(2)  # Print state 2 times per second
         self.joint_states = [0.0] * len(self.joints_names)
 
@@ -45,15 +46,29 @@ class DynamicRouter:
         data = msg.data  # Extract array from Float64MultiArray
 
         # Ensure the received data length matches the number of publishers
-        if len(data) != len(self.publishers):
-            rospy.logwarn(f"Received {len(data)} values, but expected {len(self.publishers)}. Ignoring message.")
+        if len(data) <= len(self.publishers):
+            rospy.logwarn(f"Received {len(data)} values, but expected at least {len(self.publishers)}. Ignoring message.")
             return
 
         # Publish each float to the corresponding topic
+        j = 0
         for i in range(len(data)):
-            limited_value = self.clamp(data[i], self.joints_limits[i][0], self.joints_limits[i][1])
-            self.publishers[i].publish(Float64(limited_value))
-            self.joint_states[i] = limited_value
+            if i in self.coupled_fingers : 
+                j1_value = self.clamp(data[j], self.joints_limits[j][0], self.joints_limits[j][1])
+                self.joint_states[j] = j1_value
+                j = j +1
+                j2_value = self.clamp(data[j], self.joints_limits[j][0], self.joints_limits[j][1])
+                self.joint_states[j] = j2_value
+                j = j+1
+                limited_value = j1_value + j2_value # sum the two joints values
+                self.publishers[i].publish(Float64(limited_value))            
+
+            else :
+                limited_value = self.clamp(data[j], self.joints_limits[i][0], self.joints_limits[i][1])
+                self.joint_states[j] = limited_value
+                j = j+1
+                self.publishers[i].publish(Float64(limited_value))
+
 
     def print_joint_states(self):
         while not rospy.is_shutdown():
