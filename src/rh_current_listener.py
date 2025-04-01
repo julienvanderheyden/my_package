475162ec382c599dae05298f691a1543db3db2d8
julienvdh <julien.vanderheyden@student.uliege.be>
2025-04-01 16:55:57 +0200

    add data csv saving to current listener

--- src/rh_current_listener.py
+++ src/rh_current_listener.py
@@ -3,9 +3,22 @@
 import rospy
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus
 from std_msgs.msg import Float64MultiArray, Float64
+import csv
+import os
+import argparse
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 
 class CurrentListener:
-    def __init__(self):
+    def __init__(self, store_data, csv_filename):
         # Initialize the ROS node
         rospy.init_node("current_listener", anonymous=True)
 
@@ -26,6 +39,14 @@ class CurrentListener:
         rospy.Subscriber("/diagnostics", DiagnosticArray, self.callback)
 
         rospy.loginfo(f"Listening to /diagnostics and publishing currents on {self.output_topic}")
+
+        self.store_data = store_data
+        self.csv_file = csv_filename 
+
+        if self.store_data:
+            self.init_csv()
+            rospy.loginfo(f"Saving joint values into {self.csv_file}")
+
 
     def callback(self, msg):
         """
@@ -48,9 +69,29 @@ class CurrentListener:
             currents_msg.data = currents
             self.current_pub.publish(currents_msg)
 
+            if self.store_data : 
+                self.write_csv(rospy.get_time(), currents)
+
+    def init_csv(self):
+        file_exists = os.path.isfile(self.csv_file)
+        with open(self.csv_file, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            if not file_exists:
+                writer.writerow(["Timestamp"] + [f"{name}" for name in self.motor_names])
+
+    def write_csv(self, timestamp, currents):
+        with open(self.csv_file, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([timestamp] + currents)
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("store_data", type=str2bool, help="Boolean flag to enable or disable data storage")
+    parser.add_argument("--csv_filename", type=str, default="shadowhand_currentt_data.csv", help="Optional CSV filename")
+    args = parser.parse_args()
+
     try:
-        node = CurrentListener()
+        node = CurrentListener(args.store_data, args.csv_filename)
         rospy.spin()  # Keep the node running
     except rospy.ROSInterruptException:
         pass
