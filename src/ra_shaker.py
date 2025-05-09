00a45ad4f3e581f9ba174fd038ada8007396684b
julienvdh <julien.vanderheyden@student.uliege.be>
2025-05-09 11:43:59 +0200

    shaking in progress 6

--- src/ra_shaker.py
+++ src/ra_shaker.py
@@ -1,100 +1,105 @@
 #!/usr/bin/env python3
 
 import rospy
+from std_msgs.msg import Empty
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
-def send_shake_trajectory():
-    rospy.init_node('shake_test')
+class ShakeTestNode:
+    def __init__(self):
+        rospy.init_node('shake_test')
 
-    pub = rospy.Publisher('/ra_trajectory_controller/command', JointTrajectory, queue_size=10)
+        self.joint_names = [
+            'ra_shoulder_pan_joint', 'ra_shoulder_lift_joint', 'ra_elbow_joint',
+            'ra_wrist_1_joint', 'ra_wrist_2_joint', 'ra_wrist_3_joint'
+        ]
 
-    # # Wait until publisher is connected
-    rospy.loginfo("Waiting for controller publisher...")
-    while pub.get_num_connections() == 0 and not rospy.is_shutdown():
-        rospy.sleep(0.1)
-    rospy.loginfo("Connected to controller!")
+        self.pregrasp_position = [-0.4528, -0.997, 2.278, 1.2558, 1.1071, -1.564]
+        self.grasping_position = [-0.28, -0.74, 1.66, -0.89, 1.27, -1.55]
+        self.lifting_position = [-0.285, -1.048, 1.575, -0.503, 1.27, -1.55]
+        self.shake_left = [-0.258+0.2, -1.048, 1.575, -0.503, 1.27, -1.55]
+        self.shake_right = [-0.258-0.2, -1.048, 1.575, -0.503, 1.27, -1.55]
 
-    joint_names = ['ra_shoulder_pan_joint', 'ra_shoulder_lift_joint', 'ra_elbow_joint',
-                    'ra_wrist_1_joint', 'ra_wrist_2_joint', 'ra_wrist_3_joint']
+        self.time_stages = [3.0, 1.0, 0.5]
 
-    pregrasp_position = [-0.4528, -0.997, 2.278, 1.2558, 1.1071, -1.564]
-    grasping_position = [-0.28, -0.74, 1.66, -0.89, 1.27, -1.55]
-    lifting_position = [-0.285, -1.048, 1.575, -0.503, 1.27, -1.55]
-    shake_left = [-0.258+0.2, -1.048, 1.575, -0.503, 1.27, -1.55]
-    shake_right = [-0.258-0.2, -1.048, 1.575, -0.503, 1.27, -1.55]
+        self.pub = rospy.Publisher('/ra_trajectory_controller/command', JointTrajectory, queue_size=10)
 
-    time_stages = [3.0, 1.0, 0.5]  # seconds between shakes
+        # Subscribers for triggering actions
+        rospy.Subscriber('/start_grasping', Empty, self.callback_grasping)
+        rospy.Subscriber('/start_testing', Empty, self.callback_testing)
 
-    # Send empty trajectory to initialize
-    empty_traj = JointTrajectory()
-    empty_traj.joint_names = joint_names
-    empty_traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
+        rospy.loginfo("Waiting for controller publisher...")
+        while self.pub.get_num_connections() == 0 and not rospy.is_shutdown():
+            rospy.sleep(0.1)
+        rospy.loginfo("Connected to controller.")
 
-    pub.publish(empty_traj)
-    rospy.sleep(0.5)  # give the controller time to react
-    rospy.loginfo("Initialized empty trajectory.")
+        self.send_empty_trajectory()
+        self.send_trajectory(self.pregrasp_position, 2.0)
+        rospy.loginfo("Moved to pregrasp position.")
 
-    rospy.loginfo(f"Starting lifting test")
-    traj = JointTrajectory()
-    traj.joint_names = joint_names
-    lifting_point = JointTrajectoryPoint()
-    lifting_point.positions = lifting_position
-    lifting_point.time_from_start = rospy.Duration(2.0)
-    traj.points.append(lifting_point)
-    traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
-    pub.publish(traj)
-    rospy.sleep(2.0)  # give the controller time to lift
-    rospy.loginfo("object lifted : waiting 5 seconds")
-    rospy.sleep(5.0)  # wait for 5 seconds
-    rospy.loginfo("Lifting test complete.")
+    def send_empty_trajectory(self):
+        empty_traj = JointTrajectory()
+        empty_traj.joint_names = self.joint_names
+        empty_traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
+        self.pub.publish(empty_traj)
+        rospy.sleep(0.5)
+        rospy.loginfo("Initialized empty trajectory.")
 
-    for stage_index, time_step in enumerate(time_stages):
-        rospy.loginfo(f"Starting shake stage {stage_index + 1} with time_step = {time_step:.2f}s")
-
-        # Create trajectory message
+    def send_trajectory(self, position, duration):
         traj = JointTrajectory()
-        traj.joint_names = joint_names
-        traj.points = []
-
-        current_time = 0.0  
-
-        for i in range(3):  # Repeat 3 times per stage
-            # Shake left
-            p1 = JointTrajectoryPoint()
-            p1.positions = shake_left
-            current_time += time_step
-            p1.time_from_start = rospy.Duration(current_time)
-            traj.points.append(p1)
-
-            # Shake right
-            p2 = JointTrajectoryPoint()
-            p2.positions = shake_right
-            current_time += time_step
-            p2.time_from_start = rospy.Duration(current_time)
-            traj.points.append(p2)
-        
+        traj.joint_names = self.joint_names
+        point = JointTrajectoryPoint()
+        point.positions = position
+        point.time_from_start = rospy.Duration(duration)
+        traj.points.append(point)
         traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
-        pub.publish(traj)
+        self.pub.publish(traj)
+        rospy.sleep(duration)
 
-        rospy.sleep(current_time + 1.0)
+    def callback_grasping(self, msg):
+        rospy.loginfo("Received start_grasping signal.")
+        self.send_trajectory(self.grasping_position, 2.0)
+        rospy.loginfo("Grasping movement complete.")
 
-    rospy.loginfo("Shaking test complete.")
+    def callback_testing(self, msg):
+        rospy.loginfo("Received start_testing signal.")
+        self.run_test_sequence()
 
-    # Send the hand to the grasping position
-    rospy.loginfo("Sending hand to grasping position.")
-    traj = JointTrajectory()
-    traj.joint_names = joint_names
-    grasping_point = JointTrajectoryPoint()
-    grasping_point.positions = grasping_position
-    grasping_point.time_from_start = rospy.Duration(2.0)
-    traj.points.append(grasping_point)
-    traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
-    pub.publish(traj)
-    rospy.sleep(2.0)  # give the controller time to grasp
-    rospy.loginfo("Grasping test complete.")
+    def run_test_sequence(self):
+        rospy.loginfo("Lifting object...")
+        self.send_trajectory(self.lifting_position, 2.0)
+        rospy.loginfo("Waiting 5 seconds...")
+        rospy.sleep(5.0)
+
+        for stage_index, time_step in enumerate(self.time_stages):
+            rospy.loginfo(f"Shake stage {stage_index + 1} with time_step = {time_step:.2f}s")
+            traj = JointTrajectory()
+            traj.joint_names = self.joint_names
+            traj.points = []
+
+            current_time = 0.0
+            for _ in range(3):
+                p1 = JointTrajectoryPoint()
+                p1.positions = self.shake_left
+                current_time += time_step
+                p1.time_from_start = rospy.Duration(current_time)
+                traj.points.append(p1)
+
+                p2 = JointTrajectoryPoint()
+                p2.positions = self.shake_right
+                current_time += time_step
+                p2.time_from_start = rospy.Duration(current_time)
+                traj.points.append(p2)
+
+            traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
+            self.pub.publish(traj)
+            rospy.sleep(current_time + 1.0)
+
+        rospy.loginfo("Shaking complete. Returning to grasping position.")
+        self.send_trajectory(self.grasping_position, 2.0)
 
 if __name__ == '__main__':
     try:
-        send_shake_trajectory()
+        node = ShakeTestNode()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
