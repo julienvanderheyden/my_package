8e389793b65d946dee18ffbfd18edefded1bbe44
julienvdh <julien.vanderheyden@student.uliege.be>
2025-05-09 10:55:15 +0200

    shaking in progress

--- src/ra_shaker.py
+++ src/ra_shaker.py
@@ -9,10 +9,10 @@ def send_shake_trajectory():
     pub = rospy.Publisher('/ra_trajectory_controller/command', JointTrajectory, queue_size=10)
 
     # # Wait until publisher is connected
-    # rospy.loginfo("Waiting for controller publisher...")
-    # while pub.get_num_connections() == 0 and not rospy.is_shutdown():
-    #     rospy.sleep(0.1)
-    # rospy.loginfo("Connected to controller!")
+    rospy.loginfo("Waiting for controller publisher...")
+    while pub.get_num_connections() == 0 and not rospy.is_shutdown():
+        rospy.sleep(0.1)
+    rospy.loginfo("Connected to controller!")
 
     joint_names = ['ra_shoulder_pan_joint', 'ra_shoulder_lift_joint', 'ra_elbow_joint',
                     'ra_wrist_1_joint', 'ra_wrist_2_joint', 'ra_wrist_3_joint']
@@ -20,7 +20,7 @@ def send_shake_trajectory():
     shake_left = [0.25, -0.92, 1.6, -0.72, 1.6, -1.5]  
     shake_right = [-0.25, -0.92, 1.6, -0.72, 1.6, -1.5]
 
-    time_stages = [10.0, 5.0, 3.0]  # seconds between shakes
+    time_stages = [3.0, 2.0, 1.0]  # seconds between shakes
 
     for stage_index, time_step in enumerate(time_stages):
         rospy.loginfo(f"Starting shake stage {stage_index + 1} with time_step = {time_step:.2f}s")
@@ -46,7 +46,8 @@ def send_shake_trajectory():
             current_time += time_step
             p2.time_from_start = rospy.Duration(current_time)
             traj.points.append(p2)
-
+        
+        traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
         pub.publish(traj)
 
         rospy.sleep(current_time + 1.0)
