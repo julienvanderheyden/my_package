c657a3e8c74c9fc24f47447dd295285e21a9416c
julienvanderheyden <bilou.vanderheyden@gmail.com>
2025-10-22 09:35:53 +0000

    tests for power sphere grasp

--- src/arm_controller.py
+++ src/arm_controller.py
@@ -53,7 +53,7 @@ class UR10eMoveItController:
         ]
 
         self.orientations = [
-            [pi, -pi/2, 0.1],
+            [pi/2, 0, pi/2],
             [pi, -pi/2, 0],
             [pi, -pi/2, 0],
             [pi, -pi/2, 0],
