962b39cf657b8ff9d1a0fcb37c91fa349f46c825
julienvanderheyden <bilou.vanderheyden@gmail.com>
2025-11-28 13:04:14 +0100

    git test 2

--- src/arm_controller.py
+++ src/arm_controller.py
@@ -37,7 +37,7 @@ class UR10eMoveItController:
         ### THIS SHOULD BE CHANGED FOR EACH TEST ###
     
         #grasp type 
-        self.grasp_type = 1 # 1 : medium wrap, 2 : power sphere, 3 : lateral pinch
+        self.grasp_type = 2 # 1 : medium wrap, 2 : power sphere, 3 : lateral pinch
 
         self.parameters = [0.01, 0.015, 0.02, 0.0225, 0.025, 0.0275, 0.03, 0.035, 0.04] # cylinders
         # self.parameters = [0.01, 0.015, 0.015, 0.015, 0.02, 0.02, 0.02, 0.025, 0.03] # prisms 
